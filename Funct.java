7090c89acf5c58d0dcffe9aff57be2d021e9a994
VivekMallickLxmp <vivekmallick123@outlook.com>
2025-05-23 22:16:49 +0530

    Modified file

--- Funct.java
+++ Funct.java
@@ -1,45 +1,17 @@
 @FunctionalInterface
 interface A           
-// SAM = Single abstract method interface. or Functional interface i.e an interface that has only one method. 
 {
- // void show();
-   // void run();
   void show(int... values); 
-  // var args i.e we can pass multiple values in this method.
-//    void show(int i, int j);  for multiple method we have to use the {}.
-//    void show(int i);  // if we have multiple method then we have to use the {}.
-   // what if we want to pass some value here, then we also pass the value where we put the Lambda expression.
+  
 }
 
-// class B implements A
-// {
-//     // Annotation are the extra information which we provide to our JVM .
-//    @Override
-//    public void show()
-//    {
-//      System.out.println("in B show");
-//    }
-// }
 
 
 public class Funct 
 {
     public static void main(String[] args) 
     {
-        // A obj = (int i) -> System.out.println("In a show");
-        // A obj = i -> System.out.println("In a show");
-        // A obj = i -> System.out.println("in a show " + i );
-       //  A obj =(i , j) -> System.out.println("in a show " + i + " , " + j);
-
-    // here we also have to pass the value & we can also pass multiple value without showing the data type & the ().
-                // if we have multiple statement then we can use this {} or otherwise we can avoid it.  
-          // Lambda expression = syntactically we reduce the code by this Lambda expression & behind the scene the public void show(){
-        //.           } is there 
-        // A obj = new A()
-        // {
-            
-        //     public void show()
-    //}
+       
             
          A obj = (values) ->
         {
